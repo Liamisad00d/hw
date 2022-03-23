f275e884cbc034daa2a05539115a223493f13b5c
Liamisad00d <102241822+Liamisad00d@users.noreply.github.com>
2022-03-23 11:26:58 -0400

    Verymuchyes.cs

---
+++ yes.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collection.Generic;
+namespace Indvar
+{
+    class newclass
+    {
+         static void Main(string[] args)
+         {
+              public class mouseMovementVariables
+             {
+
+
+            double xPos = 0;
+            double yPos = 0;
+            double zPos;
+
+        bool mouseMoveX;
+        bool mouseMoveY;
+        bool spaceBarIsPressed;
+
+        double mouseXChange;
+        double mouseYChange;
+        double yVelocity;
+
+
+        if ( mouseMoveX = true )
+        {
+            double newMousexPos = xPos + mouseXChange;
+        }
+        if (mouseMoveY = true)
+        {
+            double newMouseyPos = yPos + mouseXChange;
+        }
+        if (spaceBarIsPressed = true)
+        {
+            yVelocity += 10;
+        }
+        public class threeDMovementVariables
+        {
+            double xVelocity = 0;
+            double zVelocity = 0;
+            bool wKeyPress;
+            bool aKeyPress;
+            bool sKeyPress;
+            bool dKeyPress;
+        }
+        if (wKeyPress = true)
+        {
+            xVelocity += 10;
+        }
+        if (aKeyPress = true)
+        {
+            zVelocity += -90;
+              }
+              if (sKeyPress = true)
+              {
+                  xVelocity += -10;
+              }
+              if (dKeyPress = true)
+              {
+                  zVelocity += 90;
+              }
+              // controls
+              List<string> newList = new List<string>();
+              newList.Add("newString");
+              newList.Add("newMethods");
+              newList.Add("L");
+              newList.Add("G");
+              Console.WriteLine(newList.Count);
+              // first list
+              foreach ( string n in newList )
+              {
+                  Console.WriteLine(n);
+              }
+// simple for each loop
+              List<int> = newIntList = new List<int>
+              {
+                  1, 2, 3, 4,
+              }
+// int list
+              for ( int i, i > 0, i++)
+              {
+                  Console.WriteLine(i);
+              }
+              List<double> doubleList = new List<double>();
+              doubleList.Add(123);
+              doubleList.Add(921.1);
+              doubleList.Add(8272.01);
+              bool removed = doubleList.Remove(2);
+              Console.WriteLine(removed);
+              Console.WriteLine(doubleList.Find(3));
+              List<double> anotherList = new List<double>();
+              anotherList.Add(1);
+              anotherList.Add(232);
+              anotherList.Add(734);
+              anotherList.Add(9823);
+              anotherList.Add(99.9);
+              anotherList.Add(1902);
+              anotherList.Add(983);
+              anotherList.Add(674);
+              anotherList.Add(202);
+              anotherList.Remove(983);
+
+              foreach( int a in anotherList)
+              {
+                  anotherList.Add(a++);
+              }
+              List<char> charList = new List<char>
+              {
+                  a,
+                  b,
+                  c,
+                  d,
+                  e,
+                  f,
+                  g
+              }
+
+              foreach (char z in charList)
+              {
+                  Console.WriteLine(z);
+              }
+              charList.Add(L);
+    }}}
+
+}
